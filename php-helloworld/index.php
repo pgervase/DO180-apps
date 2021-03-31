3e75c0978bef4aa7666333da8a15794d94f86f8e
Student User <student@workstation.lab.example.com>
2021-03-31 13:14:35 -0400

    fixed quotes

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming! pg 0331"\n";
-print "A change is coming! pg 0331"\n";
+print "A change is coming! pg 0331\n";
+print "A change is coming! pg 0331\n";
 ?>
