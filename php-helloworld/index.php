f15a53ead7d46b2ce257478a8bc45323a1fcea34
Student User <student@workstation.lab.example.com>
2021-03-31 13:37:44 -0400

    another change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming! pg 0331\n";
-print "A change is coming! pg 0331\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n<br />";
+print "A change is coming! pg 0331\n<br />";
+print "how will i get this to show????\n";
 ?>
